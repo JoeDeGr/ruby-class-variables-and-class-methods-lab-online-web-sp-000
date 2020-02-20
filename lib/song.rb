ebd468e9af591ee44ba7da2962adc240e14992a5
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-20 00:15:29 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+require 'pry'
+
+class Song
+
+  @@count = 0
+  def self.count
+    @@count
+  end
+
+  @@artists = []
+  def self.artists
+    @@artists
+  end
+
+  @@genres = []
+  def self.genres
+    @@genres
+  end
+#binding.pry
+  def new (name, artist, genre)
+    # @@count += 1
+    # @@artist << artist
+    # @@genres << genre
+    # @name = name
+  end
+#attr_reader :name, :artist, :genre
+
+def count
+  @@count
+end
+def artists
+  @@artists
+end
+def genres
+  @@genres
+end
+
+def genre_count
+  @@genres.count
+end
+end
